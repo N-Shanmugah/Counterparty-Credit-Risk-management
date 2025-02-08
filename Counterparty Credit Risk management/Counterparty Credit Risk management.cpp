a451abda4ad94cdd9411d8d570b9ac7c0b5c8c9d
Shanmugah Nagaraju <shanmugah.nagaraju@gmail.com>
2025-02-08 22:51:55 +0800

    Part 1/4 done

--- Counterparty Credit Risk management/Counterparty Credit Risk management.cpp
+++ Counterparty Credit Risk management/Counterparty Credit Risk management.cpp
@@ -1,4 +1,39 @@
 #include <iostream>
+#include <string>
+using std::string;
+using std::cout;
+using std::endl;
+
+class Loan {
+private:
+	string ID;
+	double Notional = 0.0;
+	double APR = 0.0;
+	double Rank = 0.0;
+
+public:
+	double getNotional() const {
+		return Notional;
+	}
+
+	double getAPR() const {
+		return APR;
+	}
+
+	double getRank() const {
+		return Rank;
+	}
+
+	string getID() const {
+		return ID;
+	}
+};
+
+bool operator<(Loan& const l1, Loan& const l2) {
+
+	return (l1.getNotional() * l1.getRank()) > (l2.getNotional() * l2.getRank());
+}
+
 
 int main()
 {
